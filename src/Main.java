44884fc60925b9321a6fe01f9a4b8c19a0bdc96d
edu09 <edu09@EDUPC09.n00d01.kis.co.jp>
2020-04-22 13:19:39 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("うらかみ");
+
+		int tuki = 4;
+		int hi = 22;
+		System.out.println("今日は" + tuki +"月");
+		System.out.println(hi +"日です");
+
+		double p =3.14;
+		System.out.println("円周率は" + p +"です");
+	}
+
+}
