890b3b70a7adf9e158787d82d67961b77b45a09d
edu09 <edu09@EDUPC09.n00d01.kis.co.jp>
2020-04-22 14:29:42 +0900

    足し算

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,38 @@ public class Main {
 
 		double p =3.14;
 		System.out.println("円周率は" + p +"です");
+
+		int a;
+		int b;
+		a =20;
+		b =a+5;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("今日は\"4月\"です");
+		System.out.println("\\100");
+
+		System.out.println("C:\\に\"フォルダ\"を作成");
+
+		int num=10;
+		int amari =10%3;
+		System.out.println(amari);
+
+		num +=1;
+		num++;
+		System.out.println(num);
+
+		double d = 3;
+		int n =(int)3.0;
+		String s ="ベスト" +3;
+
+		int kokugo = 70;
+		int sansu = 75;
+		double heikin = (kokugo+sansu)/2.0;
+		System.out.println(heikin);
+		
+		int num1=3;
+		int num2=5;
+		System.out.println("足し算の結果は"+(num1+num2));
 	}
 
 }
