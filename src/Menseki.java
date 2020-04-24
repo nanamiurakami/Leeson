1775d690fd8b9c72fbd0626259aa7dfec48c659d
edu09 <edu09@EDUPC09.n00d01.kis.co.jp>
2020-04-24 11:47:10 +0900

    とりあえず午前中の分_1

---
+++ src/Menseki.java
@@ -0,0 +1,24 @@
+
+public class Menseki {
+
+	public static void main(String[] args) {
+		
+		//半径10の円の面積
+		enMenseki(10);
+		
+		
+
+	}
+	//1.メソッド enMensekiを作る
+	    //引数 int hankei
+		//hankei*hankei*3.14 を表示
+		
+	
+	public static void enMenseki(int hankei) {
+		System.out.println(hankei*hankei*3.14);
+	}
+
+	//2.半径1から5の円の面積を表示（for）
+	
+	//3.半径3 8 20 の円の面積を表示（配列）
+}
